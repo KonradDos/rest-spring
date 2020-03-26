47247539b7d9a5ea75097fb50ecc1d8421cefba2
konradDos <konradacm@gmail.com>
2020-03-26 19:43:55 +0100

    added query for updating products

--- src/main/java/pl/konrad/rest/springrest/contoller/ProductsController.java
+++ src/main/java/pl/konrad/rest/springrest/contoller/ProductsController.java
@@ -1,12 +1,14 @@
-package pl.konrad.rest.springrest.rest;
+package pl.konrad.rest.springrest.contoller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.konrad.rest.springrest.model.Product;
 import pl.konrad.rest.springrest.respository.ProductRepository;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController()
 @RequestMapping("/products")
@@ -16,12 +18,25 @@ public class ProductsController {
     ProductRepository repository;
 
     @GetMapping()
-    public List<Product> get() {
+    public List<Product> getAll() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getOne(@PathVariable("id") Long id) {
+        return repository.findById(id);
     }
 
     @PostMapping
     public Product addProduct(@Valid @RequestBody Product product) {
         return repository.save(product);
+    }
+
+    @PatchMapping
+    public void patchProduct(@Valid @RequestBody Product product, HttpServletResponse httpServletResponse) {
+        int responseCode = repository.updateProductForId(product.getId(), product.getName(), product.getDescription(), product.getPrice(), product.getWeight(), product.getCategory());
+        if (responseCode == 1) httpServletResponse.setStatus(204);
+        else httpServletResponse.setStatus(400);
+
     }
 }
--- src/main/java/pl/konrad/rest/springrest/respository/ProductRepository.java
+++ src/main/java/pl/konrad/rest/springrest/respository/ProductRepository.java
@@ -1,7 +1,19 @@
 package pl.konrad.rest.springrest.respository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pl.konrad.rest.springrest.model.Category;
 import pl.konrad.rest.springrest.model.Product;
 
+import javax.transaction.Transactional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("update Product p set p.name = :name, p.description = :description, p.price = :price, p.weight = :weight, p.category = :category where p.id = :id")
+    int updateProductForId(@Param("id") Long id, @Param("name") String name, @Param("description") String description, @Param("price") double price, @Param("weight") double weight, @Param("category") Category category);
+
 }
